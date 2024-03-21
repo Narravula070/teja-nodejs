dc09ff680e3fe58a2fcd9cb79bb31c6a359b1d40
Narravula muni sai teja <124333841+Narravula070@users.noreply.github.com>
2024-03-21 17:05:17 +0530

    Update index.js
    hlo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hii Buddy This is Teja....!");
+  res.send("Hii Buddy This is Teja.....!");
 });
 
 app.get("/me", (req, res) => {
